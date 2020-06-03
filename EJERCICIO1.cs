a7bc5657809a93ce9001979a5774db617f7f06a3
ltaverasG <64043686+ltaverasG@users.noreply.github.com>
2020-06-02 21:14:51 -1100

    Initial commit

---
+++ EJERCICIO1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+/*Crear un programa que pida el nombre de un dia de la semana e identifique si es fin de semana o no. En caso de error, indicarlo */
+namespace EXAMEN_ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// variables 
+            int Dia = 0;
+            int lunes = 1;
+            int martes = 2;
+            int miercoles = 3;
+            int jueves = 4;
+            int viernes = 5;
+            int sabado = 6;
+            int domingo = 7;
+
+            // ponemos encabezado 
+            Console.WriteLine("Ingrese un dia de la semana del 1 al 7 ");
+
+            //ingresamos dato en la variable dia
+            Dia = Convert.ToInt32(Console.ReadLine());
+
+            // condiciones.
+
+            //Error 
+            if (Dia > 7)
+            {
+                Console.WriteLine("ERROR EXCEDIO EL LIMITE...");
+            }
+
+            //Fin de semana
+            if (Dia > 5)
+            {
+                if (Dia == 6)
+                {
+                    Console.WriteLine("El dia es sabado y cae en el fin de semana");
+                }
+                else
+                {
+                    Console.WriteLine("El dia es Domingo y cae en el fin de semana");
+                }
+            }
+            // dia de semana 
+            if (Dia < 6)
+            {
+                if (Dia == 1)
+                {
+                    Console.WriteLine(" El dia es Lunes ");
+                }
+
+                if (Dia == 2)
+                {
+                    Console.WriteLine(" El dia es Martes ");
+                }
+
+                if (Dia == 3)
+                {
+                    Console.WriteLine(" El dia es Miercoles ");
+                }
+
+                if (Dia == 4)
+                {
+                    Console.WriteLine(" El dia es Jueves ");
+                }
+
+                if (Dia == 5)
+                {
+                    Console.WriteLine(" El dia es Viernes ");
+                }
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
---
+++ EXAMEN ejercicio 2/EXAMEN ejercicio 2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Security.Cryptography;
+/*Pedir al usuario el precio de un producto y la forma de pagar
+* (efectivo o tarjeta) si la forma de pago es mediante tarjeta, 
+* pedir el numero de tarjeta (12 digitos). Ojo, validar que el valor ingresado en el sea positivo. */
+
+namespace EXAMEN_ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// Variables 
+            int precio = 0;
+            int efectivo = 0;
+            int tarjeta ;
+            int opcion = 0;
+            int T;
+
+
+            //dato del precio del producto 
+            Console.WriteLine("INGRESE EL PRECIO DEL PRODUCTO");
+            precio = Convert.ToInt32(Console.ReadLine());
+
+            //forma de pago 
+            Console.WriteLine("INGRESE FORMA DE PAGO");
+            Console.WriteLine("1- efectivo 2- tarjeta");
+            opcion = Convert.ToInt32(Console.ReadLine());
+            // condiciones 
+            //efectivo 
+            if (opcion == 1)
+            {
+                Console.WriteLine("Ingrese el total a pagar de: " + precio);
+                int I = Convert.ToInt32(Console.ReadLine());
+                if (I != precio)
+                {
+                    Console.WriteLine(" ERROR INTENTE DE NUEVO ");
+                }
+            }
+            //tarjeta
+            if (opcion == 2)
+            {
+                Console.WriteLine("INGRESE EL NUMERO DE LA TARJETA");
+               // bucle para que ingrese los 12 digitos y la condicion para evitar que halla un numero negativo.
+                while (efectivo <12)
+                {
+                    efectivo++;
+                    tarjeta = Convert.ToInt32(Console.ReadLine());
+                    if (tarjeta < 0)
+                    {
+                        Console.WriteLine("ERROR INTENTE DE NUEVO ");
+                    }
+                }
+               
+                    
+            }
+
+
+        }
+
+    }
+ } 
